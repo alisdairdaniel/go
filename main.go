a5c1e90441f4eb61c8027ffc33727fa923d51242
alisdairdaniel <117979215+alisdairdaniel@users.noreply.github.com>
2022-11-17 17:42:20 -0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+import (
+    "time"
+    "fmt"
+)
+
+func main() {
+    
+    j :=0
+    for i :=0 ; i < 9999999 ; i++ {
+    
+        j += i
+    }
+    fmt.println(j)
+    cost := sime.Since(start)
+    fmr.printf('Costco: %s', cost)
+
+    }
